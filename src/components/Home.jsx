ebd50426a93e89d8c4a16c0f8f15462a5ab55789
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2024-08-16 15:26:52 +0530

    Web3 Contract functions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import { whatsMyAddress, check, getUserUpvotes, getUserDownvotes, upvoteFeedback, downvoteProductFeedback, getBalance, addProductFeedback, getProductFeedback } from '../lib/Web3'
 
+const address = '0xFaaeC23fc39F082bC45834ca01B89c6808D6C5a0';
 export default function Home() {
+  
+  // const testAddInProductsList = async () => {
+  //   let text = 'aakash jain';
+  //   let iteration = 1;
+
+  //   while (iteration < 50) {
+  //       try {
+  //           console.log(`Iteration ${iteration}: Trying with text length ${text.length}`);
+  //           await addInProductsList(text, address);
+  //           console.log(`Success with text length: ${text.length}`);
+            
+  //           // Increase the text length
+  //           text += 'a'; // You can append any character or string
+  //           iteration++;
+  //       } catch (error) {
+  //           console.error(`Error encountered at text length ${text.length}:`, error);
+  //           break; // Stop when an error occurs
+  //       }
+  //   }
+
+  //   console.log('Test complete.');
+  // };
+
+
   return (
     <div>
       Home
       <div className='bg-green-500 h-10 w-20'></div>
+      <div className='flex flex-col gap-3'>
+        <button onClick={() => whatsMyAddress(address)}>what is my fucking address</button>
+        <button onClick={() => getBalance(address)} className='border border-black '>balance</button>
+        <button onClick={() => addProductFeedback(address)} className='border border-black '>final add rating</button>
+        <button onClick={() => getProductFeedback(address)} className='border border-black '>final get rating</button>
+        <button onClick={() => upvoteFeedback(address, "0xFaaeC23fc39F082bC45834ca01B89c6808D6C5a0")} className='border border-black '>upvote</button>
+        <button onClick={() => downvoteProductFeedback(address,  "0xFaaeC23fc39F082bC45834ca01B89c6808D6C5a0")} className='border border-black '>downvote</button>
+        <button onClick={() => getUserDownvotes(address)} className='border border-black '>my downvotes</button>
+        <button onClick={() => getUserUpvotes(address)} className='border border-black '>my upvotes</button>
+        <button onClick={() => check(address, "0xFaaeC23fc39F082bC45834ca01B89c6808D6C5a0")} className='border border-black '>check</button>
+      </div>
     </div>
   )
 }
---
+++ src/lib/Web3.js
@@ -0,0 +1,112 @@
+import {Web3} from 'web3';
+import { providerUrl, abi, contractAddress } from './constants';
+
+const web3 = new Web3(providerUrl);
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+export const getBalance = async (address) => {
+	try {
+		const balance = await web3.eth.getBalance(address);
+		console.log('Balance:', balance);
+	} catch (error) {
+		console.error('Error getting balance:', error);
+	}
+}
+
+export const whatsMyAddress = async (address) => {
+	try {
+		const addressOutput= await contract.methods.myAddress().call({
+			from: address
+		});
+
+		console.log('My address: ', addressOutput);
+	} catch (error) {
+		console.error('Error getting balance:', error);
+	}
+}
+
+export const addProductFeedback = async (address, productId, feedback, rating) => {
+	try {
+		await contract.methods.addProductFeedback(
+      productId, 
+      feedback,
+      rating
+    ).send({
+			from: address,
+      gas: 20000000
+		});
+	} catch (error) {
+		console.error('Error adding feedback:', error);
+	}
+}
+
+export const getProductFeedback = async (address, productId) => {
+	try {
+		const feedbacks = await contract.methods.getProductFeedbacks(
+      productId
+    ).call({
+			from: address,
+      gas: 20000000
+		});
+
+		console.log('Feedbacks: ', feedbacks);
+	} catch (error) {
+		console.error('Error getting feedbacks:', error);
+	}
+}
+
+export const getUserUpvotes = async (address) => {
+	try {
+		const upvotes = await contract.methods.getUserUpvotes().call({
+			from: address,
+      gas: 20000000
+		});
+
+		console.log('My upvotes: ', upvotes);
+	} catch (error) {
+		console.error('Error getting user upvotes:', error);
+	}
+}
+
+export const getUserDownvotes = async (address) => {
+	try {
+		const downvotes = await contract.methods.getUserDownvotes().call({
+			from: address,
+      gas: 20000000
+		});
+
+		console.log('My downvotes: ', downvotes);
+	} catch (error) {
+		console.error('Error getting user downvotes:', error);
+	}
+}
+
+export const upvoteFeedback = async (address, productId, op) => {
+	try {
+		await contract.methods.upvoteProductFeedback(
+      productId,
+      op.toLowerCase()
+    ).send({
+			from: address,
+      gas: 2000000
+		});
+
+	} catch (error) {
+		console.error('Error upvoting:', error);
+	}
+}
+
+export const downvoteProductFeedback = async (address, productId, op) => {
+	try {
+		await contract.methods.downvoteProductFeedback(
+      productId,
+      op.toLowerCase()
+    ).send({
+			from: address,
+      gas: 2000000
+		});
+
+	} catch (error) {
+		console.error('Error downvoting:', error);
+	}
+}
